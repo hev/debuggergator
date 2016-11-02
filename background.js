7755fe81242d1a914a7bee7d84b3dc9c29c984e7
Mark Kollasch <mkollasch@pivotal.io>
2016-11-02 08:31:24 -0600

    Implement new UX
    - Use regex-based scraping
    - Configure scraping prefix and suffix inline
    - Show scraping results in checklist in popup

--- background.js
+++
@@ -1,8 +0,0 @@
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // No tabs or host permissions needed!
-  console.log('Turning ' + tab.url + ' red!');
-  chrome.tabs.executeScript({
-    file: 'scrapeContent.js'
-  });
-});
